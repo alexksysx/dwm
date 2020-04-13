ab42c37bba553b320913afaf85ea564a91176de2
alexksysx <leha.bar.ru@gmail.com>
2020-04-13 19:39:47 +0300

    Tabbed on now + pertag

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 /*  Modes after showtab_nmodes are disabled.                                */
 enum showtab_modes { showtab_never, showtab_auto, showtab_nmodes, showtab_always};
 static const int showtab			= showtab_auto;        /* Default tab bar show mode */
-static const int toptab				= False;               /* False means bottom tab bar */
+static const int toptab				= True;               /* False means bottom tab bar */
 
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
